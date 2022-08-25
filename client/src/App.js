1e7860024d33a4af6e736ea2731d4438356b1e97
ryan-kim-dev <ryankim.h.dev@gmail.com>
2022-08-25 16:41:52 +0900

    클라이언트 임시 완성, 요청 처리부분 확인 필요

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,103 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import styled from 'styled-components';
+import axios from 'axios';
 
 const App = () => {
-  return <div>App</div>;
+  const [answerLists, setAnswerLists] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get('/list')
+      .then(res => {
+        console.log(res);
+        console.log(res.data);
+        setAnswerLists(res.data);
+      })
+      .catch(err => console.log(`${err}`));
+  }, [answerLists]);
+
+  const [data, setData] = useState({
+    name: '',
+    title: '',
+    message: '',
+  });
+
+  const onChange = e => {
+    const { name, value } = e.target;
+    setData(prev => ({
+      ...prev,
+      [name]: value,
+    }));
+  };
+
+  const onSubmit = e => {
+    e.preventDefault();
+    axios
+      .post('/upload', data)
+      .then(res => console.log(res.data))
+      .catch(err => console.log(`${err}`));
+  };
+
+  return (
+    <Container>
+      <form action="/upload/post" onChange={onChange} onSubmit={onSubmit}>
+        <div>
+          <label htmlFor="name">이름</label>
+          <input
+            id="name"
+            name="name"
+            type="text"
+            placeholder="이름을 입력하세요"
+          />
+        </div>
+        <div>
+          <label htmlFor="title">제목</label>
+          <input
+            id="title"
+            name="title"
+            type="text"
+            placeholder="제목을 입력하세요"
+          />
+        </div>
+        <div>
+          <label htmlFor="message">본문</label>
+          <textarea
+            id="message"
+            name="message"
+            type="text"
+            placeholder="본문을 입력하세요"
+          />
+        </div>
+        <div>
+          <input type="submit" value="제출" />
+        </div>
+      </form>
+      <Lists>
+        {answerLists.map(item => {
+          return (
+            <ListItem>
+              <div>{item.name}</div>
+              <div>{item.title}</div>
+              <div>{item.message}</div>
+            </ListItem>
+          );
+        })}
+      </Lists>
+    </Container>
+  );
 };
 
 export default App;
+
+const Container = styled.div`
+  width: 100%;
+  height: max-content;
+`;
+
+const Lists = styled.div`
+  width: 500px;
+  height: 1000px;
+  border: 1px solid black;
+`;
+
+const ListItem = styled.div``;
