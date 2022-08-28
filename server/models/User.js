55ea7820714aeebc68f90707a774d14a9071d4d4
ryan-kim-dev <ryankim.h.dev@gmail.com>
2022-08-28 16:06:56 +0900

    feat: auth 미들웨어 생성, 사용하여 클라이언트와 db의 토큰을 비교하여 인증 허가 여부 처리, auth 라우터 구현

--- server/models/User.js
+++ server/models/User.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 // 비밀번호 암호화 - 참조: https://juni-official.tistory.com/161
 const bcrypt = require('bcrypt');
 const saltRounds = 10; // salt를 이용해서 비밀번호를 암호화 - 10자리로
+const jwt = require('jsonwebtoken');
 
 const userSchema = mongoose.Schema({
   name: {
@@ -53,6 +54,47 @@ userSchema.pre('save', function (next) {
   }
   return next();
 });
+
+// * 비밀번호 일치 여부 조회
+userSchema.methods.comparePassword = function (plainPassword, cb) {
+  // (평문 비밀번호, 콜백함수)
+  bcrypt.compare(plainPassword, this.password, function (err, isMatch) {
+    if (err) return cb(err);
+    return cb(null, isMatch);
+  });
+};
+
+// * 토큰 생성
+// 에러: throw new Error('Expected "' + parameterName + '" to be a plain object.');
+// 대응: user._id.toHexString()
+// 기존: user._id
+
+userSchema.methods.generateToken = function (cb) {
+  // json web token을 이용해서 토큰 생성하기
+  const user = this;
+  const token = jwt.sign(user._id.toHexString(), 'secretToken'); // user._id 와 'secretToken' 을 합쳐서 jwt 토큰을 생성함.
+  user.token = token;
+  user.save(function (err, user) {
+    if (err) return cb(err);
+    cb(null, user);
+  });
+};
+
+// * 가져온 토큰 복호화 하기
+userSchema.statics.findByToken = function (token, cb) {
+  const user = this;
+
+  // 토큰 디코딩 - json 웹 토큰 사용법 참고
+  jwt.verify(token, 'secretToken', function (err, decoded) {
+    // 유저의 아이디를 이용해서 유저를 찾은 다음,
+    // 클라이언트에서 가져온 token과 db에 보관된 토큰이 일치하는지 확인.
+    // findOne: 몽고디비 메서드
+    user.findOne({ _id: decoded, token: token }, function (err, user) {
+      if (err) return cb(err);
+      cb(null, user);
+    });
+  });
+};
 
 const User = mongoose.model('User', userSchema);
 
--- server/server.js
+++ server/server.js
@@ -3,12 +3,15 @@ const app = express();
 const port = 5000;
 const path = require('path');
 const cors = require('cors');
+const cookieParser = require('cookie-parser');
 // 몽고디비 연결
 const mongoose = require('mongoose');
 // 몽고디비 환경변수 연결
 const config = require('./config/keys');
 // 정의한 Model 가져오기
 const { User } = require('./models/User');
+// 폴더를 분리해서 작성해둔 인증 처리하는 함수 가져오기
+const { auth } = require('./middleware/auth');
 
 // * 1. 미들웨어 사용 부분
 // Client의 빌드된 정적파일 사용
@@ -22,6 +25,8 @@ app.use(cors());
 // urlencoded는 url 형식의 데이터 전달을 의미 (application/x-www-form-urlencoded)
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+// 토큰을 쿠키에 담기 위해 쿠키파서 사용. 참고: https://velog.io/@heony/cookie-parser
+app.use(cookieParser());
 
 // * 2. 응답 부분
 
@@ -29,7 +34,8 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '../client/build/index.html'));
 });
 
-app.post('/register', (req, res) => {
+// * 2.1 회원가입 요청의 응답
+app.post('/api/users/register', (req, res) => {
   // 회원가입시 클라이언트 단에서 입력된 정보를 가져와 데이터베이스에 저장한다.
   // req.body에 json 객체로 키-값 이 들어가있다.
   const user = new User(req.body); // 정의한 모델을 불러와 요청 안의 데이터로 새 인스턴스 생성
@@ -41,6 +47,58 @@ app.post('/register', (req, res) => {
   });
   // save: 몽고디비의 메서드 - 콜백함수로 에러, 저장된 데이터를 매개변수로 받는다.
   // userInfo = ( user = new User(req.body) )
+});
+
+// * 2.2 로그인 요청의 응답
+// 1. 로그인 요청으로 온 이메일 주소가 데이터베이스에 존재하는지 확인한다.
+// 2. 이메일 주소가 데이터베이스에 있다면 비밀번호의 일치 여부를 확인한다.
+// 3. 이메일과 비밀번호가 모두 일치하면 토큰을 생성한다.
+app.post('/api/users/login', (req, res) => {
+  User.findOne({ email: req.body.email }, (err, user) => {
+    if (!user) {
+      return res.json({
+        loginSuccess: false,
+        message: '가입되지 않은 사용자입니다. 이메일 주소를 확인해주세요',
+      });
+    }
+    // 요청된 이메일이 데이터베이스에 존재할 경우 비밀번호 일치 여부 확인.
+    user.comparePassword(req.body.password, (err, isMatch) => {
+      if (!isMatch) {
+        return res.json({
+          loginSuccess: false,
+          message: '비밀번호가 틀렸습니다.',
+        });
+      }
+
+      // 비밀번호가 일치하는 경우 - 토큰을 생성한다.
+      user.generateToken((err, user) => {
+        if (err) return res.status(400).send(err);
+        // 토큰을 저장하는 곳은 쿠키, 세션스토리지, 로컬스토리지 다양함. 여기서는 쿠키에 저장해본다.
+        res
+          .cookie('x_auth', user.token)
+          .status(200)
+          .json({ loginSuccess: true, userId: user._id });
+      });
+    });
+  });
+});
+
+// * 2.3 인증페이지 요청 응답
+// role이 1이면 관리자, 0이면 일반 유저인 경우로 한다.
+app.get('/api/users/auth', auth, (req, res) => {
+  // './middleware/auth.js'의 auth 함수에서 코드가 종료되면서 next() 호출하므로
+  // 미들웨어를 실행시키고 다시 나머지 코드를 실행해서 응답을 보내줌.
+  // 미들웨어에서 예외처리를 해두었기 때문에 next()후 여기로 돌아온다면
+  // 인증이 성공한 경우이다.
+  res.status(200).json({
+    _id: req.user._id,
+    isAdmin: req.user.role === 0 ? false : true,
+    isAuth: true,
+    name: req.user.name,
+    email: req.user.email,
+    role: req.user.role,
+    photoURL: req.user.photoURL,
+  });
 });
 
 app.listen(port, () => {
